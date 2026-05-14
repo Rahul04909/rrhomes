524a2726ccc150e1eee7936678553e4dc870a598
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-05-14 15:58:46 +0530

    updated about us

--- includes/footer.php
+++ includes/footer.php
@@ -26,7 +26,7 @@
                 <h4 class="col-title">Quick Links</h4>
                 <ul class="footer-links">
                     <li><a href="../../index.php">Home</a></li>
-                    <li><a href="../../about-us">About Us</a></li>
+                    <li><a href="../../about-us.php">About Us</a></li>
                     <!-- <li><a href="#">Projects</a></li> -->
                     <!-- <li><a href="#">Specifications</a></li> -->
                     <li><a href="../../contact-us.php">Contact Us</a></li>
